17609ecfaaaa26aa8849d2af25d5cb15acba97d9
JMDahle <47642064+jmdahle@users.noreply.github.com>
2020-02-16 14:14:34 -0600

    Updates

---
+++ js/init.js
@@ -0,0 +1,27 @@
+// Materialize scrollspy
+document.addEventListener('DOMContentLoaded', () => {
+    let elems = document.querySelectorAll('.scrollspy');
+    let options = {};
+    let instances = M.ScrollSpy.init(elems, options);
+});
+
+// Materialize parallax
+document.addEventListener('DOMContentLoaded', () => {
+    let elems = document.querySelectorAll('.parallax');
+    let options = {};
+    let instances = M.Parallax.init(elems, options);
+});
+
+//Materialize pushpin
+document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.pushpin');
+    let options = {top: 43, offset: 43};
+    let instances = M.Pushpin.init(elems, options);
+});
+
+//Materialize select
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    let options = {};
+    var instances = M.FormSelect.init(elems, options);
+});
---
+++ precache-manifest.1545d388d5b5941dccf94c82e8423f68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5ab49f9bac2e1cf8185e011f87df0c",
+    "url": "/casascounseling/index.html"
+  },
+  {
+    "revision": "88abc12086732ca6b93c",
+    "url": "/casascounseling/static/css/main.2daecb09.chunk.css"
+  },
+  {
+    "revision": "ece99ea3852687a62781",
+    "url": "/casascounseling/static/js/2.05c8d15f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/casascounseling/static/js/2.05c8d15f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88abc12086732ca6b93c",
+    "url": "/casascounseling/static/js/main.164e0204.chunk.js"
+  },
+  {
+    "revision": "e7a244e0108f9162b601",
+    "url": "/casascounseling/static/js/runtime-main.bf4c9979.js"
+  }
+]);
